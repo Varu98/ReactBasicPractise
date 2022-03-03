a2fcf7b07de6aed4d100a8fd6e13f35c68364cc4
Varu98 <vardaan.agarwal.98@gmail.com>
2022-03-03 20:51:13 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [change, setChange] = useState('');
+  const [like, setLike] = useState(false);
+  const [theirName, setTheirName] = useState('Hooman!');
+  const [theirAge, setTheirAge] = useState(18);
+
+  const clickHandler = () => setCount(count + 1);
+  const changeHandler = (e) => setChange(e.target.value);
+  const likeHandler = (e) => setLike(e.target.checked);
+
+  const inputNameHandler = (e) => setTheirName(e.target.value);
+  const inputAgeHandler = () => setTheirAge(theirAge + 1);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button onClick={clickHandler}>You Have Pressed {count} Times</button>
+
+      <h1>Input</h1>
+      <p>Edit the Input Text</p>
+      <input
+        onChange={changeHandler}
+        type="text"
+        placeholder="Type Something Here"
+      />
+      <p>{change}</p>
+      <h1>Check Input</h1>
+
+      <label>
+        <input type="checkbox" checked={like} onChange={likeHandler} /> You{' '}
+        {like ? 'like this' : 'Did not like this'}
+      </label>
+      <h1>Both Combined</h1>
+      <label>
+        <input
+          placeholder={theirName}
+          onChange={inputNameHandler}
+          type="text"
+        />
+      </label>
+
+      <div>
+        <button onClick={inputAgeHandler}>Increment Age</button>
+      </div>
+
+      <p>
+        Hello {theirName} Your Age Is {theirAge}
+      </p>
     </div>
   );
 }
